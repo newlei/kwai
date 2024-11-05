93310f247e8e42fbed7f3e13edba5f1a3dda4635
陈雷 <newlei@gmail.com>
2024-11-05 11:13:27 +0800

    add some results from Server

--- sync.sh
+++ sync.sh
@@ -4,6 +4,6 @@ git add *
 # git commit -m 'add some code from Mac'
 git commit -m 'add some results from Server'
 git pull --rebase origin master   #domnload data
-git push origin master            #upload data
+git push origin master --force          #upload data
 git stash pop
 # sleep 3s #Just for more stable, you can remove this.
