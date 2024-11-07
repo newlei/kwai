9652126d85bff2ba66701bfe54eaced7813c1c32
lei <chenlei.hfut@gmail.com>
2024-11-07 16:25:53 +0800

    add some code from Mac

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 git status  
 git add *  
-# git commit -m 'add some code from Mac'
-git commit -m 'add some results from Server'
+git commit -m 'add some code from Mac'
+# git commit -m 'add some results from Server'
 git pull --rebase origin main   #domnload data
 git push origin main --force          #upload data
 git stash pop
