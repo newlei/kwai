4f1f8fa19d2aa0cad9aae20d9d8a4fb779fcfc42
lei <chenlei.hfut@gmail.com>
2024-11-07 16:37:40 +0800

    add some code from Mac

--- data_kg_get_att.py
+++ data_kg_get_att.py
@@ -13,13 +13,7 @@ data_interaction = pd.read_csv(file_name, usecols=['user_id','photo_id','poi_id'
 print(data_interaction.size)
 
 
-chunksize = 10 ** 6
-file_photo = '../photo_pdate_20241104.csv'
-photo_list =[]
-for chunk in pd.read_csv(file_photo,  usecols=['photo_id','poi_id','poi_name','poi_city_name','photo_type','city_name','photo_cate_type','photo_second_cate_type'], chunksize=chunksize, sep='|', lineterminator='\n'):
-    photo_list.append(chunk) 
 
-print(len(photo_list))
 
 file_user = '../user_pdate_20241104.csv'
 user_att = pd.read_csv(file_user, usecols=['user_id','photo_id','time_second','poi_id','label','play_duration','poi_page_stay_time'], sep='|')
@@ -35,4 +29,14 @@ user_att.rename(columns={'goods_id': 'poi_id'}, inplace=True)
 print(poi_att.size)
 
 pdb.set_trace()
+
+
+
+chunksize = 10 ** 6
+file_photo = '../photo_pdate_20241104.csv'
+photo_list =[]
+for chunk in pd.read_csv(file_photo,  usecols=['photo_id','poi_id','poi_name','poi_city_name','photo_type','city_name','photo_cate_type','photo_second_cate_type'], chunksize=chunksize, sep='|', lineterminator='\n'):
+    photo_list.append(chunk) 
+
+print(len(photo_list))
 
