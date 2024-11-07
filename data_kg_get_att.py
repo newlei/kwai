72f5bd1d4af857b2acc41487a9fcb4fbe9821127
lei <chenlei.hfut@gmail.com>
2024-11-07 16:05:29 +0800

    add some results from Server

--- data_kg_get_att.py
+++ data_kg_get_att.py
@@ -8,6 +8,11 @@ file_name = '../data_process/core10/data_interaction_final.csv'
 data_interaction = pd.read_csv(file_name, usecols=['user_id','photo_id','poi_id','time_second'], sep='|')
 print(data_interaction.size)
 
+file_photo = '../photo_pdate_20241104.csv'
+photo_att = pd.read_csv(file_photo, usecols=['photo_id','poi_id','poi_name','poi_city_name','photo_type','city_name','photo_cate_type','photo_second_cate_type'], sep='|', lineterminator='\n')
+
+pdb.set_trace()
+
 
 file_user = '../user_pdate_20241104.csv'
 user_att = pd.read_csv(file_user, usecols=['user_id','photo_id','time_second','poi_id','label','play_duration','poi_page_stay_time'], sep='|')
