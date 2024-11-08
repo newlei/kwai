9cb2796f01b3da83be529cfaef5c626e15a3904a
lei <chenlei.hfut@gmail.com>
2024-11-08 16:06:17 +0800

    add some code from Mac

--- data_kg_json_llm.py
+++ data_kg_json_llm.py
@@ -30,13 +30,13 @@ for index, row in data_interaction_u_att.iterrows():
 
 poi_att_dict = dict()
 #去重复的
-data_interaction_poi_att = data_interaction_poi_att.drop_duplicates(subset='user_id') 
+data_interaction_poi_att = data_interaction_poi_att.drop_duplicates(subset='poi_id') 
 #|user_id|photo_id|time_second|poi_id|poi_name|category_name|cate_2_name|cate_1_name|province_name|city_name|brand_name
 for index, row in data_interaction_poi_att.iterrows():
     # print(index) # 输出每行的索引值
-    user_id = row['user_id']
-    if user_id not in poi_att_dict:
-        poi_att_dict[user_id] = "名称是"+str(row['poi_name'])+",类型是"+str(row['category_name'])+","+str(row['cate_2_name'])+","+str(row['cate_1_name'])+",所在地是"+str(row['city_name'])
+    poi_id = row['poi_id']
+    if poi_id not in poi_att_dict:
+        poi_att_dict[poi_id] = "名称是"+str(row['poi_name'])+",类型是"+str(row['category_name'])+","+str(row['cate_2_name'])+","+str(row['cate_1_name'])+",所在地是"+str(row['city_name'])
 
 
 data = []
