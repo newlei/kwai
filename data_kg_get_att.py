cd3f9885f9e6cbd596f133a4239f4402017e8385
lei <chenlei.hfut@gmail.com>
2024-11-08 15:34:27 +0800

    add some code from Mac

--- data_kg_get_att.py
+++ data_kg_get_att.py
@@ -14,37 +14,39 @@ print('data_interaction:',data_interaction.shape)
 
 
 
-chunksize = 10 ** 6
-file_photo = '../photo_pdate_20241104.csv'
-photo_list =[]
-flag = 0
-for chunk in pd.read_csv(file_photo,  usecols=['photo_id','poi_id','poi_name','poi_city_name','photo_type','city_name','photo_cate_type','photo_second_cate_type'], chunksize=chunksize, sep='|', lineterminator='\n'):
-    photo_list.append(chunk) 
-    flag+=1
-    if flag ==1:
-        data_pid = chunk
-    elif flag>1:
-        data_pid = pd.concat([data_pid, chunk], axis=0)
-    data_pid = data_pid.drop_duplicates(subset='photo_id')
-    print(data_pid.shape,flag)
+# chunksize = 10 ** 6
+# file_photo = '../photo_pdate_20241104.csv'
+# photo_list =[]
+# flag = 0
+# for chunk in pd.read_csv(file_photo,  usecols=['photo_id','poi_id','poi_name','poi_city_name','photo_type','city_name','photo_cate_type','photo_second_cate_type'], chunksize=chunksize, sep='|', lineterminator='\n'):
+#     photo_list.append(chunk) 
+#     flag+=1
+#     if flag ==1:
+#         data_pid = chunk
+#     elif flag>1:
+#         data_pid = pd.concat([data_pid, chunk], axis=0)
+#     data_pid = data_pid.drop_duplicates(subset='photo_id')
+#     print(data_pid.shape,flag)
 
-print(len(photo_list))
-print(data_pid.shape)
+# print(len(photo_list))
+# print(data_pid.shape)
 
-merged_pidatt = pd.merge(data_interaction, data_pid, on=['photo_id'], how='inner')
-print('merged_pidatt:',merged_pidatt.shape) 
+# merged_pidatt = pd.merge(data_interaction, data_pid, on=['photo_id'], how='inner')
+# print('merged_pidatt:',merged_pidatt.shape) 
 
-file_name = '../data_process/core'+str(10)+'/data_interaction_final_cat_p_att.csv'
-merged_pidatt.to_csv(file_name, sep='|') 
+# file_name = '../data_process/core'+str(10)+'/data_interaction_final_cat_p_att.csv'
+# merged_pidatt.to_csv(file_name, sep='|') 
 
-pdb.set_trace()
+# pdb.set_trace()
 
 
 
 
 file_user = '../user_pdate_20241104.csv'
-user_att = pd.read_csv(file_user, usecols=['user_id','photo_id','time_second','poi_id','label','play_duration','poi_page_stay_time'], sep='|')
-user_att.rename(columns={'user_id': 'user_id', 'photo_id': 'u_gender','time_second': 'u_age','poi_id': 'u_age_part','label': 'u_city','poi_page_stay_time': 'u_region'}, inplace=True)
+column_names = ["user_id", "u_gender", "u_age", "u_age_part","u_city","u_province","u_country","u_north","u_region"]
+user_att = pd.read_csv(file_user, names=column_names, header=None
+# user_att = pd.read_csv(file_user, usecols=['user_id','photo_id','time_second','poi_id','label','play_duration','poi_page_stay_time'], sep='|')
+# user_att.rename(columns={'user_id': 'user_id', 'photo_id': 'u_gender','time_second': 'u_age','poi_id': 'u_age_part','label': 'u_city','poi_page_stay_time': 'u_region'}, inplace=True)
 print('user_att',user_att.shape)
 
 # poi_id|poi_name|category_id|category_name|cate_2_id|cate_2_name|cate_1_id|cate_1_name|country|province_id|province_name|city_id|city_name|district_id|district_name|town_name|brand_name|is_busi_goods|brand_level_reco|collect_poi_user_num
---
+++ data_kg_json_llm.py
@@ -0,0 +1,59 @@
+import json
+import numpy as np 
+import pandas as pd 
+import pdb
+
+
+
+file_name = '../data_process/core10/data_interaction_final.csv'
+data_interaction = pd.read_csv(file_name, usecols=['user_id','photo_id','poi_id','time_second'], sep='|')
+
+file_name = '../data_process/core10/data_interaction_final_cat_u_att.csv'
+data_interaction_u_att = pd.read_csv(file_name, sep='|')
+
+file_name = '../data_process/core10/data_interaction_final_cat_poi_att.csv'
+data_interaction_poi_att = pd.read_csv(file_name, sep='|')
+
+# file_name = '../data_process/core10/data_interaction_final_cat_p_att.csv'
+# data_interaction_photo_att = pd.read_csv(file_name, sep='|')
+
+u_att_dict = dict()
+#去重复的
+data_interaction_u_att = data_interaction_u_att.drop_duplicates(subset='user_id') 
+#u_gender|u_age|u_age_part|u_city|play_duration|u_region
+for index, row in data_interaction_u_att.iterrows():
+    # print(index) # 输出每行的索引值
+    user_id = row['user_id']
+    if user_id not in u_att_dict:
+        u_att_dict[user_id] = "性别是"+str(row['u_gender'])+",年龄是"+str(row['u_age'])+",居住地是"+str(row['u_city'])+",居住地属于中国"+str(row['u_region'])
+
+
+poi_att_dict = dict()
+#去重复的
+data_interaction_poi_att = data_interaction_poi_att.drop_duplicates(subset='user_id') 
+#|user_id|photo_id|time_second|poi_id|poi_name|category_name|cate_2_name|cate_1_name|province_name|city_name|brand_name
+for index, row in data_interaction_poi_att.iterrows():
+    # print(index) # 输出每行的索引值
+    user_id = row['user_id']
+    if user_id not in poi_att_dict:
+        poi_att_dict[user_id] = "商品名称是"+str(row['poi_name'])+",商品类型是"+str(row['category_name'])+","+str(row['cate_2_name'])+","+str(row['cate_1_name'])+",商品所在地是"+str(row['city_name'])
+
+
+data_interaction = data_interaction.groupby('user_id')["poi_id",].apply(list).reset_index(name="poi_id")
+for index, row in data_interaction.iterrows():
+
+
+
+
+
+data.append({
+        "instruction": instruction,
+        "input": text
+    })
+
+output_file = '../data_process/core'+str(10)+'/data_kg_llm.json'
+
+with open(output_file, 'w', encoding='utf-8') as f:
+    for item in data:
+        json.dump(item, f, ensure_ascii=False)
+        f.write('\n')
