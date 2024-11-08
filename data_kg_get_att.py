10aaffe6316768bb36d6dcbfcfefdac34d4cf55e
lei <chenlei.hfut@gmail.com>
2024-11-08 09:20:51 +0800

    add some code from Mac

--- data_kg_get_att.py
+++ data_kg_get_att.py
@@ -14,10 +14,9 @@ print(data_interaction.size)
 
 
 
-
 file_user = '../user_pdate_20241104.csv'
 user_att = pd.read_csv(file_user, usecols=['user_id','photo_id','time_second','poi_id','label','play_duration','poi_page_stay_time'], sep='|')
-user_att.rename(columns={'user_id': 'user_id', 'photo_id': 'gender','time_second': 'age','poi_id': 'age_part','label': 'city','play_duration': 'region'}, inplace=True)
+user_att.rename(columns={'user_id': 'user_id', 'photo_id': 'u_gender','time_second': 'u_age','poi_id': 'u_age_part','label': 'u_city','poi_page_stay_time': 'u_region'}, inplace=True)
 print('user_att',user_att.size)
 
 # file_photo = '../photo_pdate_20241104.csv'
@@ -26,6 +25,13 @@ print('user_att',user_att.size)
 file_poi = '../poi_pdate_20241104.csv'
 poi_att = pd.read_csv(file_poi, sep='|')
 print('poi_att',poi_att.size)
+poi_att['poi_id'] = poi_att['poi_id'].fillna(0.0).astype('int', errors='ignore')
+
+# df.astype({'value': 'int'}, errors='ignore')
+# poi_att[poi_att[poi_att['poi_id']=='seemly假发'].index]
+
+merged_table = pd.merge(data_interaction, user_att, on=['user_id'], how='inner')
+merged_table2 = pd.merge(data_interaction, poi_att, on=['poi_id'], how='inner')
 
 pdb.set_trace()
 
