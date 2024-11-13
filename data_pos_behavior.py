d1bad6b1ac6fb6ee865075ccfa9f114f479b92fa
lei <chenlei.hfut@gmail.com>
2024-11-13 17:00:44 +0800

    add some code from Mac

--- data_pos_behavior.py
+++ data_pos_behavior.py
@@ -9,6 +9,7 @@ from joblib import Parallel, delayed
 from itertools import combinations 
 import scipy
 from scipy.sparse import csr_matrix 
+import itertools
 
 file_name = '../data_process/core10/data_interaction_final.csv'
 data_interaction = pd.read_csv(file_name, usecols=['user_id','photo_id','poi_id','time_second'], sep='|')
