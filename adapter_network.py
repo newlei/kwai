2ca07e623d93b99a193a0d7f0e58e9971487e06f
lei <chenlei.hfut@gmail.com>
2024-11-13 14:13:52 +0800

    add some code from Mac

--- adapter_network.py
+++ adapter_network.py
@@ -83,9 +83,8 @@ class embData(data.Dataset):
         # u=[] u_pos=[] u_copy = [] u_neg = []
         u_id = self.all_id[idx] 
         pos_id_list  = self.pair_dict[u_id]
-        pos_id = np.random.choice(pos_id_list,1)#random.sample(set(pos_id_list),1)
-        print(pos_id,'----')
-        pdb.set_trace()
+        pos_id = np.random.choice(pos_id_list,1)[0]#random.sample(set(pos_id_list),1)
+        print(pos_id,'----') 
 
         u = self.emb_dict[u_id]
         u_pos = self.emb_dict[pos_id]
