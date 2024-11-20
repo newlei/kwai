e0005dc6ef9dc89029e8926277c311aab55181e2
lei <chenlei.hfut@gmail.com>
2024-11-20 15:03:59 +0800

    add some code from Mac

--- data_kg_get_att.py
+++ data_kg_get_att.py
@@ -44,9 +44,12 @@ print('data_interaction:',data_interaction.shape)
 
 
 file_user = '../llm_graph_data/user_fix_pdate_20241112.csv' 
-column_names = ["user_id", "u_gender", "u_age", "u_age_part","u_city","u_province","u_country","u_north","u_region"]
-user_att = pd.read_csv(file_user, names=column_names, header=None, sep='|')
-user_att = user_att.iloc[1: , :]
+user_att = pd.read_csv(file_user, sep='|')
+# 1039494098  3622322528    2259270636
+
+# column_names = ["user_id", "u_gender", "u_age", "u_age_part","u_city","u_province","u_country","u_north","u_region"]
+# user_att = pd.read_csv(file_user, names=column_names, header=None, sep='|')
+# user_att = user_att.iloc[1: , :]
 # user_att = pd.read_csv(file_user, usecols=['user_id','photo_id','time_second','poi_id','label','play_duration','poi_page_stay_time'], sep='|')
 # user_att.rename(columns={'user_id': 'user_id', 'photo_id': 'u_gender','time_second': 'u_age','poi_id': 'u_age_part','label': 'u_city','poi_page_stay_time': 'u_region'}, inplace=True)
 print('user_att',user_att.shape) 
