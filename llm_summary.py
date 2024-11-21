0f8021fd7b75b6774d41ac3e34d58adb0647c235
lei <chenlei.hfut@gmail.com>
2024-11-21 20:26:06 +0800

    add some code from Mac

--- llm_summary.py
+++ llm_summary.py
@@ -9,7 +9,7 @@ model_name ="Qwen/Qwen2.5-7B-Instruct-GPTQ-Int8"
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype="auto",
-    device_map="auto"
+    device_map="auto",
 )
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
