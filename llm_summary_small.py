dc767e1551fcc4f55e8d4387df29a15e6e3cc869
lei <chenlei.hfut@gmail.com>
2024-11-25 15:41:16 +0800

    add some code from Mac

--- llm_summary_small.py
+++ llm_summary_small.py
@@ -10,7 +10,7 @@ from vllm import LLM, SamplingParams
 
 # Step 1: 初始化模型
 # model_path ="Qwen/Qwen2.5-1.5B-Instruct"  
-model_path ="Qwen/Qwen2.5-3B-Instruct"
+model_path ="Qwen/Qwen2.5-7B-Instruct"
 llm = LLM(model=model_path, dtype='half')
 
 # Step 2: 定义批量输入数据
