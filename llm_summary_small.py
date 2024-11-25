df1e83cd7a1331564d93bbdfd02479bd987d1014
lei <chenlei.hfut@gmail.com>
2024-11-25 19:45:00 +0800

    add some code from Mac

--- llm_summary_small.py
+++ llm_summary_small.py
@@ -117,54 +117,55 @@ pdb.set_trace()
 exit()
 
 
+# Create an LLM.
+# model = LLM(model="intfloat/e5-mistral-7b-instruct", enforce_eager=True)
+model = LLM(model='Alibaba-NLP/gte-Qwen2-7B-instruct', enforce_eager=True, tensor_parallel_size=2)
+# Generate embedding. The output is a list of EmbeddingRequestOutputs.
 
-def last_token_pool(last_hidden_states: Tensor,
-                 attention_mask: Tensor) -> Tensor:
-    left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
-    if left_padding:
-        return last_hidden_states[:, -1]
-    else:
-        sequence_lengths = attention_mask.sum(dim=1) - 1
-        batch_size = last_hidden_states.shape[0]
-        return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
+json_res_path = '../data_process/core'+str(10)+'/train/data_kg_llm_summary.json' 
+# json_res_path = '../data_process/core'+str(10)+'/train/data_kg_llm_summary_item.json'
+elapsed_time_all = 0
+elapsed_time_count = 0
 
-def get_detailed_instruct(task_description: str, query: str) -> str:
-    return f'Instruct: {task_description}\nQuery: {query}'
+user_emb = dict()
 
+with open(json_path, 'r', encoding="utf-8") as f:
+    batch_size=0
+    batch_data=[]
+    batch_data_id = []
+    for one_data in f.readlines(): 
+        # 将josn字符串转化为dict字典
+        start_time = time.time()
+        prompt_one = json.loads(one_data)  
+        # str_in = prompt_one["data"]["instruction"]+"上下文信息："+prompt_one["data"]["input"]+"\n \n 写出总结性的回答，不包含原句重复。"
+        # batch_data.append(str_in)
 
-# Each query must come with a one-sentence instruction that describes the task
-# task = 'Given a web search query, retrieve relevant passages that answer the query'
-# queries = [
-#     get_detailed_instruct(task, 'how much protein should a female eat'),
-#     get_detailed_instruct(task, 'summit define')
-# ]
-# # No need to add instruction for retrieval documents
-# documents = [
-#     "As a general guideline, the CDC's average requirement of protein for women ages 19 to 70 is 46 grams per day. But, as you can see from this chart, you'll need to increase that if you're expecting or training for a marathon. Check out the chart below to see how much protein you should be eating each day.",
-#     "Definition of summit for English Language Learners. : 1  the highest point of a mountain : the top of a mountain. : 2  the highest level. : 3  a meeting or series of meetings between the leaders of two or more governments."
-# ]
-# input_texts = queries + documents
+        messages = [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": str(prompt_one["data"])}
+            ]
+        batch_data.append(prompt_one["data"])  
+        # batch_data.append(str(prompt_one["data"])+"\n 请用中文回答")
+        batch_data_id.append(prompt_one["user_id"])
+        if batch_size<=64:
+            batch_size+=1
+            continue
+   
+        outputs = model.encode(batch_data)
+        # Print the outputs.
+        for i in range(len(outputs))
+            user_id  = batch_data_id[i]
+            user_emb = outputs[i]
+            user_emb[user_id] = user_emb
+            print(output.outputs.embedding)  # list of 4096 floats
+        pdb.set_trace()
 
-tokenizer = AutoTokenizer.from_pretrained('Alibaba-NLP/gte-Qwen2-7B-instruct', trust_remote_code=True)
-model = AutoModel.from_pretrained('Alibaba-NLP/gte-Qwen2-7B-instruct', trust_remote_code=True)
-
-max_length = 8192
-
-# # Tokenize the input texts
-batch_dict = tokenizer(input_texts, max_length=max_length, padding=True, truncation=True, return_tensors='pt')
-# batch_dict = tokenizer([response,response,response,response], max_length=max_length, padding=True, truncation=True, return_tensors='pt')
-outputs = model(**batch_dict)
-embeddings = last_token_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
-
-# normalize embeddings
-embeddings = F.normalize(embeddings, p=2, dim=1)
-scores = (embeddings[:2] @ embeddings[2:].T) #计算向量的相似度。0-1
-print(scores.tolist())
-
-print(embeddings)#torch.Size([1, 3584])
-
-pdb.set_trace()
- 
-
-
- 
+        elapsed_time = time.time() - start_time
+        elapsed_time_all+=elapsed_time
+        elapsed_time_count+=1
+        elapsed_time_average = elapsed_time_all/elapsed_time_count
+        print('--each pair time--',elapsed_time,'---avg time--',elapsed_time_average)
+        # pdb.set_trace()
+        batch_data = []
+        batch_data_id = []
+        
