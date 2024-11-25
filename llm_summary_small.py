86c2dd6f965b0d15b3eaa8f1c1fc6984389cf233
lei <chenlei.hfut@gmail.com>
2024-11-25 15:01:20 +0800

    add some code from Mac

--- llm_summary_small.py
+++ llm_summary_small.py
@@ -23,7 +23,7 @@ batch_data = [
 sampling_params = SamplingParams(
     temperature=0.7,
     top_p=0.9,
-    # max_tokens=100,  # 限制生成的最大长度
+    max_tokens=1000,  # 限制生成的最大长度
 )
 
 # Step 4: 执行批量推理
