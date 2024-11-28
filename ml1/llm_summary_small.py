4a3f452cde7d7a870f087cb56661ac5586fcf64a
lei <chenlei.hfut@gmail.com>
2024-11-28 11:06:00 +0800

    add some code from Mac

---
+++ ml1/llm_summary_small.py
@@ -0,0 +1,138 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import pdb
+import json
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from transformers import AutoTokenizer, AutoModel
+import time
+from vllm import LLM, SamplingParams
+
+
+# CUDA_VISIBLE_DEVICES=4  python llm_summary_small.py
+
+
+# Step 1: 初始化模型
+# model_path ="Qwen/Qwen2.5-1.5B-Instruct"  
+model_path ="../Qwen2.5-7B-Instruct"
+llm = LLM(model=model_path)#, dtype='half', tensor_parallel_size=2) 
+
+# Step 2: 定义批量输入数据
+batch_data = [
+    "请简述量子计算的基本原理。",
+    "给出关于人工智能伦理的三条建议。",
+    "如何评价最近的大模型技术发展？",
+]
+
+# Step 3: 设置采样参数
+sampling_params = SamplingParams(
+    temperature=0.7,
+    top_p=0.8,
+    top_k=20,
+    max_tokens=500,  # 限制生成的最大长度
+    stop=["<|endoftext|>"]
+)
+
+# # Step 4: 执行批量推理
+# results = llm.generate(batch_data, sampling_params)
+
+# # Step 5: 输出结果
+# for i, output in enumerate(results):
+#     print(f"输入: {batch_data[i]}")
+#     print(f"生成结果: {output.outputs[0].text}\n")
+
+ 
+
+count=0
+input_texts =[] 
+# json_path = '../data/data_llm_summary_user.json'
+json_path = '../data/data_llm_summary_item.json' 
+elapsed_time_all = 0
+elapsed_time_count = 0
+
+res_data = []
+with open(json_path, 'r', encoding="utf-8") as f:
+    # 读取所有行 每行会是一个字符串
+    batch_size=0
+    batch_data=[]
+    batch_data_id = []
+    start_time = time.time()
+    for one_data in f.readlines(): 
+        count+=1
+        # 将josn字符串转化为dict字典
+        prompt_one = json.loads(one_data)  
+        # str_in = prompt_one["data"]["instruction"]+"上下文信息："+prompt_one["data"]["input"]+"\n \n 写出总结性的回答，不包含原句重复。"
+        # batch_data.append(str_in)
+
+        messages = [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": str(prompt_one["data"])}
+            ]
+        batch_data.append(messages)   
+        # batch_data_id.append(prompt_one["user_id"])
+        batch_data_id.append(prompt_one["poi_id"])
+        if batch_size<=2046:
+            batch_size+=1
+            continue
+   
+        # response = llm.generate(batch_data, sampling_params)
+        # llm.generate(str_in, sampling_params)
+        # conversation = [{"role": "system", "content": str(prompt_one["data"])}]
+        response = llm.chat(batch_data, sampling_params)
+ 
+        for i, output in enumerate(response): 
+            # res_data.append({
+            #     "user_id":  batch_data_id[i],
+            #     "data": output.outputs[0].text
+            # }) 
+
+            res_data.append({
+                "poi_id":  batch_data_id[i],
+                "data": output.outputs[0].text
+            }) 
+
+        elapsed_time = time.time() - start_time
+        elapsed_time_all+=elapsed_time
+        elapsed_time_count+=1
+        elapsed_time_average = elapsed_time_all/elapsed_time_count
+        print('--each pair time--',elapsed_time,'---avg time--',elapsed_time_average,'---count---',count)
+        # pdb.set_trace()
+        batch_data = []
+        batch_data_id = []
+        batch_size = 0
+        start_time = time.time() 
+        # if count>2:
+        #     break
+        # # pdb.set_trace()
+        # count+=1
+
+    if  batch_size>0:
+        print("laster batch", batch_size)
+        response = llm.chat(batch_data, sampling_params)
+        for i, output in enumerate(response): 
+            # res_data.append({
+            #     "user_id":  batch_data_id[i],
+            #     "data": output.outputs[0].text
+            # }) 
+            
+            res_data.append({
+                "poi_id":  batch_data_id[i],
+                "data": output.outputs[0].text
+            }) 
+
+
+# json_res_path = '../data/data_llm_summary_user_summary.json'
+json_res_path = '../data/data_llm_summary_item_summary.json'
+with open(json_res_path, 'w', encoding='utf-8') as f:
+    for item in res_data:
+        json.dump(item, f, ensure_ascii=False)
+        f.write('\n')
+
+f1 = open(json_res_path, 'r', encoding="utf-8")
+for one_data in f1.readlines(): 
+    prompt_one = json.loads(one_data) 
+
+print('end')
+pdb.set_trace()
+exit()
+
