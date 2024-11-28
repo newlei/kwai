5ab5663a0a1b89cc5eea5f4b86eadfc9b3ec0e93
lei <chenlei.hfut@gmail.com>
2024-11-28 16:03:42 +0800

    add some code from Mac

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@ git status
 git add *  
 git commit -m 'add some code from Mac'
 # git commit -m 'add some results from Server'
-git pull --rebase origin main   #domnload data
-git push origin main --force          #upload data
+git pull --rebase main   #domnload data
+git push main --force          #upload data
 git stash pop
 # sleep 3s #Just for more stable, you can remove this.
